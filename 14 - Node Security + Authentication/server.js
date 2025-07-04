7e64d19cde56ed4f802c439e5fd84d0192b2ad4e
biplavpaudel123 <biplavpaudel123@gmail.com>
2025-07-04 09:56:52 +0545

    Added auth endpoints

--- 14 - Node Security + Authentication/server.js
+++ 14 - Node Security + Authentication/server.js
@@ -7,8 +7,22 @@ const express= require('express');
 const PORT=9000;
 
 const app= express();
+function checkedLoggedIn(req,res,next){
+const isLoggedIn=true;
+if (!isLoggedIn){
+return res.status(401).json({
+      eror:'You must log in!',
+});
+}
+next();
+}
 
-app.get('/secret',(req,res)=>{
+app.get('/auth/google',(req,res)=>{});
+app.get('/auth/google/callback',(req,res)=>{});
+app.get('/auth/logout',(req,res)=>{});
+
+
+app.get('/secret',checkedLoggedIn,(req,res)=>{
     return  res.send('Your secret value is 42!');
 })
 
